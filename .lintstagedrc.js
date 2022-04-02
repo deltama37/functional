a1d1be725a05861876673aacf90edeef2fc46c42
ryota misumi <3delta7@gmail.com>
2022-04-03 01:09:16 +0900

    pre-commit

---
+++ .lintstagedrc.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`;
+
+const buildPrettierCommand = (filenames) =>
+  `prettier --write ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" ")}`;
+
+const typeCheck = () => `tsc --noEmit`;
+
+module.exports = {
+  "*.{js,jsx,ts,tsx}": [buildPrettierCommand, buildEslintCommand, typeCheck],
+};
